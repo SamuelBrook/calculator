8577d44ab0777f2853913110539869e6e54e54af
Samuel Brook <samuelbhbrook@gmail.com>
2021-10-03 21:37:21 +0200

    Edit index.html and scripts.js to add some functionality to the calculator

--- scripts.js
+++ scripts.js
@@ -1,9 +1,58 @@
 
+let screenDisplay = document.querySelector("#screen");
 
+let enteredNum = "";
+
+let count = 0;
+
+let symbolPressed = "";
+
+let numEntered = "";
+
+let clicked = false;
+
+const calcButtons = document.querySelectorAll(".calc");
+
+calcButtons.forEach((calcButton) => {
+    calcButton.addEventListener("click", () => {
+        if (calcButton !== "=") {
+            if (count > 0) {
+                alert(operate(symbolPressed, 2, 2));
+                symbolPressed = calcButton.textContent;
+
+
+            }
+            else {
+                symbolPressed = calcButton.textContent;
+                count++;
+            
+            }
+        }
+    })
+})
+
+
+const padButtons = document.querySelectorAll(".num");
+
+
+padButtons.forEach((padButton) => {
+    padButton.addEventListener("click", () => {
+        enteredNum += padButton.textContent;
+        screenDisplay.textContent = enteredNum;
+    })
+});
+
+
+const equalSymbol = document.querySelector("#equals");
+
+equalSymbol.addEventListener("click", () => {
+});
 
 //calculator functions
 function operate(operator, num1, num2) {
-    add(num1, num2);
+    if (operator === "+") {
+        return add(num1, num2);
+    }
 }
  
 function add(num1, num2) {
