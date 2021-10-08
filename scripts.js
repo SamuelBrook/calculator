ac4b7d190c3eaf22b88cd437d82740235d1849f1
Samuel Brook <samuelbhbrook@gmail.com>
2021-10-08 19:13:09 +0200

    Edit index.html scripts.js and styles.css

--- scripts.js
+++ scripts.js
@@ -4,30 +4,58 @@ let screenDisplay = document.querySelector("#screen");
 let enteredNum = "";
 
 let count = 0;
+let countNum = 0;
 
 let symbolPressed = "";
 
 let numEntered = "";
 
-let clicked = false;
+let isPressed = false;
+let calcDone = false; 
+
+let savedNum1;
+let savedNum2;
+let product;
 
 const calcButtons = document.querySelectorAll(".calc");
 
 calcButtons.forEach((calcButton) => {
     calcButton.addEventListener("click", () => {
-        if (calcButton !== "=") {
-            if (count > 0) {
-                alert(operate(symbolPressed, 2, 2));
+       
+        //if +, -, / etc...
+        // if (calcButton !== "="){
+            if (calcDone === true) {
+                savedNum2 = screenDisplay.textContent;
+                product = operate(symbolPressed, savedNum1, savedNum2);
+                screenDisplay.textContent = product;
+                savedNum1 = product;
                 symbolPressed = calcButton.textContent;
-
+                isPressed = true;
 
             }
             else {
                 symbolPressed = calcButton.textContent;
-                count++;
-            
+                savedNum1 = screenDisplay.textContent;
+                calcDone = true;
+                isPressed = true;
             }
-        }
+            
+        // }
+        // else if (calcButton === "=") {
+        //     savedNum2 = screenDisplay.textContent;
+        //     // console.log(isPressed);
+        //     // if (isPressed == false) {
+        //     //     screenDisplay.textContent = savedNum2;
+        //     // }
+        //     // else if (!product) {
+        //     //     product = operate(symbolPressed, savedNum1, savedNum2);
+        //     //     screenDisplay.textContent = product;
+        //     // }
+        //     // else {
+        //     //     product = operate(symbolPressed, product, savedNum2);
+        //     //     screenDisplay.textContent = product;
+        //     // }
+        // }
     })
 })
 
@@ -37,11 +65,15 @@ const padButtons = document.querySelectorAll(".num");
 
 padButtons.forEach((padButton) => {
     padButton.addEventListener("click", () => {
+        if (isPressed == true) {
+            clearScreen();
+            enteredNum = "";
+            isPressed = false;
+        }
         enteredNum += padButton.textContent;
         screenDisplay.textContent = enteredNum;
     })
 });
-
 
 const equalSymbol = document.querySelector("#equals");
 
@@ -53,20 +85,35 @@ function operate(operator, num1, num2) {
     if (operator === "+") {
         return add(num1, num2);
     }
+    else if (operator === "-") {
+        return subtract(num1, num2);
+    }
+    else if (operator === "*") {
+        return multiply(num1, num2);
+    }
+    else if (operator === "/") {
+        return divide(num1, num2);
+    }
 }
  
 function add(num1, num2) {
-    return num1 + num2;
+    return parseInt(num1) + parseInt(num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return parseInt(num1) - parseInt(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return parseInt(num1) * parseInt(num2);
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return parseInt(num1) / parseInt(num2);
+}
+
+
+function clearScreen() {
+    // document.getElementById("screen").innerHTML = "";
+    screenDisplay.textContent = "";
 }
