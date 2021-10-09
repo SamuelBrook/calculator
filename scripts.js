49f992cd14c118a0d69697dfcd272fde137d5d7c
Samuel Brook <samuelbhbrook@gmail.com>
2021-10-09 14:28:34 +0200

    Edit index.html scripts.js and styles.css

--- scripts.js
+++ scripts.js
@@ -2,13 +2,7 @@
 let screenDisplay = document.querySelector("#screen");
 
 let enteredNum = "";
-
-let count = 0;
-let countNum = 0;
-
 let symbolPressed = "";
-
-let numEntered = "";
 
 let isPressed = false;
 let calcDone = false; 
@@ -17,103 +11,112 @@ let savedNum1;
 let savedNum2;
 let product;
 
+
+
 const calcButtons = document.querySelectorAll(".calc");
 
 calcButtons.forEach((calcButton) => {
     calcButton.addEventListener("click", () => {
-       
-        //if +, -, / etc...
-        // if (calcButton !== "="){
-            if (calcDone === true) {
-                savedNum2 = screenDisplay.textContent;
-                product = operate(symbolPressed, savedNum1, savedNum2);
-                screenDisplay.textContent = product;
-                savedNum1 = product;
-                symbolPressed = calcButton.textContent;
-                isPressed = true;
-
-            }
-            else {
-                symbolPressed = calcButton.textContent;
-                savedNum1 = screenDisplay.textContent;
-                calcDone = true;
-                isPressed = true;
-            }
-            
-        // }
-        // else if (calcButton === "=") {
-        //     savedNum2 = screenDisplay.textContent;
-        //     // console.log(isPressed);
-        //     // if (isPressed == false) {
-        //     //     screenDisplay.textContent = savedNum2;
-        //     // }
-        //     // else if (!product) {
-        //     //     product = operate(symbolPressed, savedNum1, savedNum2);
-        //     //     screenDisplay.textContent = product;
-        //     // }
-        //     // else {
-        //     //     product = operate(symbolPressed, product, savedNum2);
-        //     //     screenDisplay.textContent = product;
-        //     // }
-        // }
+        if (calcButton.textContent === "AC") { // needs edits as doesnt work properly, need to add % and +/- functionality
+            resetCalc();
+        }
+        if (symbolPressed === "/" && screenDisplay.textContent === "0") {
+            screenDisplay.textContent = "Not today";
+            enteredNum = "";
+            symbolPressed = "";
+            isPressed = false;
+            calcDone = false;
+        }
+        else if (calcButton.textContent === "=" && calcDone === true) {
+            savedNum2 = screenDisplay.textContent;
+            product = operate(symbolPressed, savedNum1, savedNum2);
+            screenDisplay.textContent = product;
+            savedNum1 = product;
+            isPressed = true;
+            calcDone = false;
+        }
+        else if (calcButton.textContent !== "=" && calcDone === true) {
+            savedNum2 = screenDisplay.textContent;
+            product = operate(symbolPressed, savedNum1, savedNum2);
+            screenDisplay.textContent = product;
+            savedNum1 = product;
+            symbolPressed = calcButton.textContent;
+            isPressed = true;
+        }
+        else if (calcButton.textContent !== "=") {
+            symbolPressed = calcButton.textContent;
+            savedNum1 = screenDisplay.textContent;
+            calcDone = true;
+            isPressed = true;
+        }
     })
 })
 
 
-const padButtons = document.querySelectorAll(".num");
 
+const padButtons = document.querySelectorAll(".num");
 
 padButtons.forEach((padButton) => {
     padButton.addEventListener("click", () => {
-        if (isPressed == true) {
+        if (screenDisplay.textContent === "Not today") {
+
+        }
+        if (isPressed === true) {
             clearScreen();
             enteredNum = "";
             isPressed = false;
         }
+        
         enteredNum += padButton.textContent;
         screenDisplay.textContent = enteredNum;
     })
 });
 
-const equalSymbol = document.querySelector("#equals");
 
-equalSymbol.addEventListener("click", () => {
-});
-
-//calculator functions
 function operate(operator, num1, num2) {
     if (operator === "+") {
-        return add(num1, num2);
+        let sum = add(num1, num2);
+        return +sum.toFixed(15);
     }
     else if (operator === "-") {
-        return subtract(num1, num2);
+        let sum = subtract(num1, num2);
+        return +sum.toFixed(15);
     }
     else if (operator === "*") {
-        return multiply(num1, num2);
+        let sum = multiply(num1, num2);
+        return +sum.toFixed(15);
     }
     else if (operator === "/") {
-        return divide(num1, num2);
+        let sum = divide(num1, num2);
+        return +sum.toFixed(15);
     }
 }
  
 function add(num1, num2) {
-    return parseInt(num1) + parseInt(num2);
+    return parseFloat(num1) + parseFloat(num2);
 }
 
 function subtract(num1, num2) {
-    return parseInt(num1) - parseInt(num2);
+    return parseFloat(num1) - parseFloat(num2);
 }
 
 function multiply(num1, num2) {
-    return parseInt(num1) * parseInt(num2);
+    return parseFloat(num1) * parseFloat(num2);
 }
 
 function divide(num1, num2) {
-    return parseInt(num1) / parseInt(num2);
+    return parseFloat(num1) / parseFloat(num2);
 }
 
 
 function clearScreen() {
-    // document.getElementById("screen").innerHTML = "";
     screenDisplay.textContent = "";
+}
+
+function resetCalc() {
+    screenDisplay.textContent = "0";
+    enteredNum = "";
+    symbolPressed = "";
+    isPressed = false;
+    calcDone = false;
 }
